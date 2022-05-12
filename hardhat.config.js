e6d68fcec1904fd9567e69191e7ef37c7882e22f
md-maruf-hossain <sany.maruf@gmail.com>
2022-05-12 13:02:23 +0600

    contract deployed to ropsten

--- hardhat.config.js
+++ hardhat.config.js
@@ -18,12 +18,15 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: "0.8.4",
+  defaultNetwork: "hardhat",
   paths: {
     artifacts: './src/artifacts',
   },
   networks: {
-    hardhat: {
-      chainId: 1337
+    hardhat: {},
+    ropsten: {
+      url: "https://ropsten.infura.io/v3/3bff9bf41e2f4734b39fe9c7cec49637",
+      accounts: [`0x87940af62e6c22a47cc15c236d5174996ca4432de8ad2023ea2a1e520284ba27`]
     }
   }
 };
