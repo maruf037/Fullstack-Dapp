ee67cd7c5244f70b6b08103fbc7716fb77654187
md-maruf-hossain <sany.maruf@gmail.com>
2022-05-15 12:01:53 +0600

    completed and deplyed token contract

---
+++ contracts/Token.sol
@@ -0,0 +1,26 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract Token {
+    string public name = "Maruf Token";
+    string public symbol = "MT";
+    uint public totalsupply = 1000000;
+    mapping(address => uint) balances;
+
+    constructor() {
+        balances[msg.sender] = totalsupply;
+    }
+
+    function transfer(address to, uint amount) external {
+        require(balances[msg.sender] >= amount, "Not enough tokens");
+        balances[msg.sender] -= amount;
+        balances[to] += amount;
+    }
+
+    function balanceOf(address account) external view returns (uint) {
+        return balances[account];
+    }
+}
